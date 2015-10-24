39d6a882b64b7f108825679e216fe583a17f8212
plai217 <philiplai217@gmail.com>
2015-10-23 22:52:16 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,22 @@
 # Implement your procedural solution here!
+def productset
+  array = []
+  1.upto(999).each do |x|
+    1.upto(999).each do |y| 
+      array << x*y
+    end
+  end
+  array.uniq.sort.reverse
+end
+
+
+def largest_palindrome_product
+  array = productset
+  array.each do |x|
+    x_array = x.to_s.split(",")
+    if x_array.all? {|y| y.reverse == y}
+      return x
+    end
+  end 
+end
+
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+
+  def productset
+    array = []
+    1.upto(999).each do |x|
+      1.upto(999).each do |y| 
+        array << x*y
+      end
+    end
+    array.uniq.sort.reverse
+  end
+
+
+  def answer
+    array = productset
+    array.each do |x|
+      x_array = x.to_s.split(",")
+      if x_array.all? {|y| y.reverse == y}
+        return x
+      end
+    end 
+  end
+
+end
